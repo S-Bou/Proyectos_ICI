988abc8cd6ebafe4b56ddd1d6fd8a4dccd568eef
S-Bou <55810268+S-Bou@users.noreply.github.com>
2021-05-23 09:09:35 +0200

    2305210909

---
+++ ProyecctoFinal_VR13/PID_Matlab/params.m
@@ -0,0 +1,12 @@
+m1=0.031; m2=0.019; m3=0.02;
+r1=0.19; r2=0.16; r3=0.11;
+p2=-0.0036; p1=0.052; p0=0;
+I=m1*r1^2+m2*r2^2+m3*r3^2;
+a2=-0.000055; a0=0.001505;
+g=9.81;
+%Punto de funcionamiento
+vo=3.855095346;
+alfa_o=0; %rad
+%Función de transferencia
+s= tf('s');
+Gs = ((2*r1*p2*vo+r1*p1)/I)/(s^2+(a0/I)*s+(m3*r3*g)/I);
